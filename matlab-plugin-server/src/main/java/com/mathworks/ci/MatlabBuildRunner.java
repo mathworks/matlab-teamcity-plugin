f0f42600b1fd7d6c88318e258878e88e518720d3
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2023-11-23 16:32:40 +0530

    Fixed issue #15

--- matlab-plugin-server/src/main/java/com/mathworks/ci/MatlabBuildRunner.java
+++ matlab-plugin-server/src/main/java/com/mathworks/ci/MatlabBuildRunner.java
@@ -1,10 +1,15 @@
 package com.mathworks.ci;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+
+import jetbrains.buildServer.serverSide.InvalidProperty;
 import jetbrains.buildServer.serverSide.PropertiesProcessor;
 import jetbrains.buildServer.serverSide.RunType;
 import jetbrains.buildServer.serverSide.RunTypeRegistry;
+import jetbrains.buildServer.util.PropertiesUtil;
 import jetbrains.buildServer.web.openapi.PluginDescriptor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -39,7 +44,16 @@ public class MatlabBuildRunner  extends RunType {
   @Nullable
   @Override
   public PropertiesProcessor getRunnerPropertiesProcessor() {
-    return null;
+    return new PropertiesProcessor() {
+      @Override
+      public Collection<InvalidProperty> process(final Map<String, String> properties) {
+        Collection<InvalidProperty> invalid = new LinkedList<InvalidProperty>();
+        if (PropertiesUtil.isEmptyOrNull(properties.get("MatlabPathKey"))) {
+          invalid.add(new InvalidProperty("MatlabPathKey", "MATLAB MATLAB root cannot be empty"));
+        }
+        return invalid;
+      }
+    };
   }
 
   @Nullable
